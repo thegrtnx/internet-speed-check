32a1f84d4a43176a24995c4fc743d1fda9b3cd42
thegrtnx <thegrtnx@duck.com>
2024-01-14 18:33:29 +0100

    ❤️ Feature: Added support for esm and cjs modules

---
+++ scripts/module-prompt.js
@@ -0,0 +1,34 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout,
+});
+
+rl.question("Which module format do you want to use? (1) CommonJS or (2) ECMAScript (ESM): ", (answer) => {
+	switch (answer.trim().toLowerCase()) {
+		case "1":
+		case "commonjs":
+			console.log("Using CommonJS module format...");
+			startApp("dist/index.js");
+			break;
+
+		case "2":
+		case "esm":
+			console.log("Using ECMAScript module format...");
+			startApp("dist/index.mjs");
+			break;
+
+		default:
+			console.log("Invalid choice. Using CommonJS module format by default...");
+			startApp("dist/index.js");
+			break;
+	}
+
+	rl.close();
+});
+
+function startApp(entryPoint) {
+	const { spawn } = require("child_process");
+	spawn("node", [entryPoint], { stdio: "inherit" });
+}
